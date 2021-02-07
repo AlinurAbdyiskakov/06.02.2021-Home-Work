b1e2cbac777e7b71bd31c8d9f1ff00102e67b4e0
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-02-07 14:25:49 +0600

    the old text delect  scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
-let click = document.querySelector("#click");
-let h1 = document.querySelector("h1");
-let sayHello = document.querySelector("#hello");
-let input = document.querySelector("#chenge");
+// let click = document.querySelector("#click");
+// let h1 = document.querySelector("h1");
+// let sayHello = document.querySelector("#hello");
+// let input = document.querySelector("#chenge");
 
 
-click.addEventListener('click', function () {
-    h1.innerText = "Hello java scripts"
-})
-sayHello.addEventListener('click', function () {
-    h1.innerText = "Hello User"
-})
-input.addEventListener('change', function () {
-    h1.innerText = input.value;
-})
-color.querySelector('change',function (){
-    h1.style.color=color.value
-})
+// click.addEventListener('click', function () {
+//     h1.innerText = "Hello java scripts"
+// })
+// sayHello.addEventListener('click', function () {
+//     h1.innerText = "Hello User"
+// })
+// input.addEventListener('change', function () {
+//     h1.innerText = input.value;
+// })
+// color.querySelector('change',function (){
+//     h1.style.color=color.value
+// })
