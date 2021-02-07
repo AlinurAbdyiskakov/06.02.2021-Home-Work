d90d09478b4ea5e3c222cdff392c4edbf82425d8
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-02-07 15:47:38 +0600

    the add stylization scripts.js

--- scripts.js
+++ scripts.js
@@ -17,27 +17,32 @@
 //     h1.style.color=color.value
 // })
 let example1 = document.querySelector("#example1");
-example1.style.backgroundColor ='rgb(253, 76, 45)';
+example1.style.backgroundColor = 'rgb(253, 76, 45)';
 example1.style.color = 'balck';
 example1.style.fontSize = '20px';
 example1.style.border = '2px solid yellow'
 
 
-let h1 =document.querySelector('h1');
-example2.addEventListener('click',function(){
+let h1 = document.querySelector('h1');
+example2.addEventListener('click', function () {
     h1.style.fontSize = (Infinity);
-   
+
 })
-example4.addEventListener('click',function(){
+example4.addEventListener('click', function () {
     let number = parseInt(h1.innerText);
     number++;
-    h1.innerText =number;
+    h1.innerText = number;
 })
-example5.addEventListener('click',function(){
+example2.addEventListener('click', function () {
+  
+    
+  
+ example2.style.fontSize = parseInt(h1.innerText);
+    fontSize++;
+    h1.innerText = fontSize;
+})
+example5.addEventListener('click', function () {
     let number = parseInt(h1.innerText);
     number--;
-    h1.innerText =number;
+    h1.innerText = number;
 })
-
-
-
