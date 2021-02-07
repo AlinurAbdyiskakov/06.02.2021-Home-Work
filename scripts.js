fe06f0212bf35d07191380efd71772f41705a057
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-02-07 13:55:57 +0600

    add click,function scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,6 @@ let click = document.querySelector("#click");
 let h1 = document.querySelector("h1");
 let sayHello = document.querySelector("#hello");
 let input =document.querySelector("#chenge");
+click.addEventListener('click',function(){
+    h1.innerText = "Hello java scripts"
+})
