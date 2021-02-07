23d9f6a49bed4d9c345c83d7d7a4e0c874a765ee
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-02-07 14:16:58 +0600

    add new stylization are the xhange atribut

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
 let click = document.querySelector("#click");
 let h1 = document.querySelector("h1");
 let sayHello = document.querySelector("#hello");
-let input =document.querySelector("#chenge");
-click.addEventListener('click',function(){
+let input = document.querySelector("#chenge");
+
+
+click.addEventListener('click', function () {
     h1.innerText = "Hello java scripts"
 })
-sayHello.addEventListener('click',function(){
+sayHello.addEventListener('click', function () {
     h1.innerText = "Hello User"
+})
+input.addEventListener('change', function () {
+    h1.innerText = input.value;
+})
+color.querySelector('change',function (){
+    h1.style.color=color.value
 })
