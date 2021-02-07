7997648aad840e509d4f361a0f47cbe553ef3042
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-02-07 13:53:08 +0600

    add new text stylization scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+let click = document.querySelector("#click");
+let h1 = document.querySelector("h1");
+let sayHello = document.querySelector("#hello");
+let input =document.querySelector("#chenge");
